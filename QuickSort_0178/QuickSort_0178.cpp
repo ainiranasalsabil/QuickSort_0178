c069f614a6ac0769c6fa8e197ceb7b415adee54c
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-03-28 11:14:59 +0700

    step 6

--- QuickSort_0178/QuickSort_0178.cpp
+++ QuickSort_0178/QuickSort_0178.cpp
@@ -50,8 +50,9 @@ void q_short(int low, int high) {
     while (i <= j) { //step 10
         //search for an element greater than pivot
         while ((arr[i] <= pivot) && (i <= high)) { // step 5
-            
-
-            }
+            i++; //step 6
+            cmp_count++;
+        }
+        
                
 
