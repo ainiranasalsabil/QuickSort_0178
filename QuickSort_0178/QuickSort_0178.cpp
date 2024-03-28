e2ff37d56dfde8bad52aaeecf9fcefd2bf8a082f
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-03-28 11:19:58 +0700

    membuat tampilan

--- QuickSort_0178/QuickSort_0178.cpp
+++ QuickSort_0178/QuickSort_0178.cpp
@@ -74,4 +74,10 @@ void q_short(int low, int high) {
 }
 
 void display() {
+    cout << "\n----------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "------------" << endl;
+
     
+               
+
