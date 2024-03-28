841b25fa6adc202f6ff79907f3c22060d20614c5
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-03-28 11:18:48 +0700

    Recursive call to sort the right sub array  step 13

--- QuickSort_0178/QuickSort_0178.cpp
+++ QuickSort_0178/QuickSort_0178.cpp
@@ -68,4 +68,11 @@ void q_short(int low, int high) {
         // recursive call to sort the left sub array
         q_short(low, j - 1); // step 12
 
-        
+        //Recursive call to sort the right sub array
+        q_short(j + 1, high); // Step 13
+    }
+}
+
+
+               
+
