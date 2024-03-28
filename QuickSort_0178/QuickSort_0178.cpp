552491e3b63e3dbb1550a1eac50227bb32ea4ecb
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-03-28 10:22:36 +0700

    mengganti komen dengan bahasa indonesia

--- QuickSort_0178/QuickSort_0178.cpp
+++ QuickSort_0178/QuickSort_0178.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 
-// array of imtegers to hold values
+// array bilangan bulat untuk menyimpan nilai
 int arr[20];
-int cmp_count = 0; // number of comparasion
-int mov_count = 0; // number of data movements
+int cmp_count = 0; // jumlah perbandingan
+int mov_count = 0; // jumlah pergerakan data
 int n;
+
 
