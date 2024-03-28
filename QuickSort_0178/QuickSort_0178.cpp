dd3f52dde7be6797a5d0ef90c9e162c537ffa93a
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-03-28 11:22:43 +0700

    membuat char ch

--- QuickSort_0178/QuickSort_0178.cpp
+++ QuickSort_0178/QuickSort_0178.cpp
@@ -85,6 +85,9 @@ void display() {
     cout << "\n\nNumber of comparasion :" << cmp_count << endl;
     cout << "Number of data movements :" << mov_count << endl;
 }
+int main() {
+    char ch;
 
+    
                
 
